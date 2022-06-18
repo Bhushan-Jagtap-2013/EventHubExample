b6efa4623297bff9ac2a38d7b1caed2168b34239
Bhushan <bhushan.jagtap.2013@gmail.com>
2022-06-18 13:47:47 -0700

    - Created a model to coffiee machine UI. - Created binding between model and view. - Implimented ModeView ViewModel desing pattern.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EventHubExample
 Sample code for EventHub.
+
+# Overview
+This project contains sample applications to test Event Hub. One of the project contain code to simulate coffee machine and send periodic sample data to Event Hub. 
+
+# Dependancies
+
+Prism.Core
---
+++ source/CoffeeMachine/CoffeeMachine.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine.UI.ViewModel
+{
+    internal class MainViewModel : BindableBase
+    {
+        private int _counterMoca;
+        private int _counterAmericano;
+        private string _city;
+        private string _serialNumber;
+
+        public MainViewModel()
+        {
+            SerialNumber = Guid.NewGuid().ToString().Substring(0, 10);
+            MakeMocaCommand = new DelegateCommand(MakeMoca);
+            MakeAmericanoCommand = new DelegateCommand(MakeAmericano);
+
+        }
+
+        private void MakeAmericano()
+        {
+            CounterAmericano++;
+        }
+
+        private void MakeMoca()
+        {
+            CounterMoca++;
+        }
+
+        public int CounterMoca {
+            get => _counterMoca;
+            set
+            {
+                _counterMoca = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int CounterAmericano {
+            get => _counterAmericano;
+            set
+            {
+                _counterAmericano = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string City { get => _city;
+            set
+            {
+                _city = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string SerialNumber
+        {
+            get => _serialNumber;
+            set
+            {
+                _serialNumber = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand MakeMocaCommand { get; }
+        public DelegateCommand MakeAmericanoCommand { get; }
+    }
+}
